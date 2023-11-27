f702f88cc348fc190621372b9bcab28f4ac5779d
AlMamunTalukder <almamuntalukder0@gmail.com>
2023-11-27 13:25:25 +0600

    details and view count

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 var jwt = require("jsonwebtoken");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 //middelwares
@@ -48,6 +48,25 @@ async function run() {
       const result = await newsCollection.find().toArray();
       res.send(result);
     });
+    //for show details
+
+    app.get("/articles/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await newsCollection.findOne(query);
+      res.send(result);
+      console.log(id);
+    });
+
+    app.post("/articles/incrementView/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await newsCollection.updateOne(query, {
+        $inc: { viewCount: 1 },
+      });
+      res.send(result);
+    });
+
     //-----------------------------------------------------
 
     // Send a ping to confirm a successful connection
